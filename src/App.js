9b3c234e22b653a130a7cc1f1fd25e14f9251a3e
nija10 <nijakharangar@gmail.com>
2024-06-22 14:30:12 +0530

    added bootstrap to project

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Nija Kharangar and is open-sourced on{" "}
-        <a href="https://github.com/nija10/sky-react" target="_blank">
-          Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Nija Kharangar and is open-sourced on{" "}
+          <a
+            href="https://github.com/nija10/sky-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
