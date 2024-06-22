4af3095ff44157c4e6f77e928ba8797d40a5d9be
nija10 <nijakharangar@gmail.com>
2024-06-22 13:14:54 +0530

    added Github repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,15 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Nija Kharangar and is open-sourced on{" "}
+        <a href="https://github.com/nija10/sky-react" target="_blank">
+          Github
+        </a>
+      </footer>
+    </div>
+  );
 }
