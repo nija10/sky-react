46a874114c1dd315590d3381fe8d6927e3aa755f
nija10 <nijakharangar@gmail.com>
2024-06-24 23:41:17 +0530

    added api

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,15 @@
 import "./Weather.css";
+import axios from "axios";
 
 export default function Weather() {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
+  let city = "London";
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
   return (
     <div className="Weather">
       <form>
