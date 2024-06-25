83e435596e9565dc5b7721f175a80e5a4fe64556
nija10 <nijakharangar@gmail.com>
2024-06-26 00:01:28 +0530

    improved date display

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -12,9 +12,12 @@ export default function FormattedDate(props) {
   ];
   let day = days[props.date.getDay()];
   let hours = props.date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
   let minutes = props.date.getMinutes();
   if (minutes < 10) {
-    minutes = `0`;
+    minutes = `0${minutes}`;
   }
   return (
     <div>
