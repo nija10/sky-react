1dbfd627f28f648211ea6a0c919ed60a6c7908f1
nija10 <nijakharangar@gmail.com>
2024-06-29 09:52:37 +0530

    moved celsius code to another file

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -16,10 +17,7 @@ export default function WeatherInfo(props) {
         <div className="col-6 detail">
           <WeatherIcon code={props.data.icon} />
           {/* <img src={props.data.iconUrl} alt={props.data.description} /> */}
-          <span className="temperature">
-            {Math.round(props.data.temperature)}
-          </span>
-          <span className="unit">°C</span>
+          <WeatherTemperature celsius={props.data.temperature} />
         </div>
         <div className="col-6">
           <ul>
