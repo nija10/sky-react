6d78192879671c43aa78c44e95a6644b60a417cb
nija10 <nijakharangar@gmail.com>
2024-07-03 20:35:38 +0530

    added forecast API

--- src/Weather.js
+++ src/Weather.js
@@ -13,6 +13,7 @@ export default function Weather(props) {
     console.log(response);
     setWeatherData({
       ready: true,
+      coordinates: response.data.coord,
       temperature: response.data.main.temp,
       date: new Date(response.data.dt * 1000),
       wind: response.data.wind.speed,
@@ -64,7 +65,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
-        <WeatherForecast />
+        <WeatherForecast coordinates={weatherData.coordinates} />
       </div>
     );
   } else {
