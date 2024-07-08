4da8465a82573ce441cc747f8570e04a1be5bb7c
nija10 <nijakharangar@gmail.com>
2024-07-08 20:57:04 +0530

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -35,7 +35,7 @@ export default function WeatherForecast(props) {
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
     let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&unites=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
 
     return null;
