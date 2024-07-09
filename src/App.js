b27481f9526ea72decb65503d214d2819eef85f6
nija10 <nijakharangar@gmail.com>
2024-07-09 18:11:41 +0530

    made changes in footer

--- src/App.js
+++ src/App.js
@@ -8,13 +8,29 @@ export default function App() {
       <div className="container">
         <Weather defaultCity="New York" />
         <footer>
-          This project was coded by Nija Kharangar and is open-sourced on{" "}
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/nija-kharangar-981548259/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Nija Kharangar{" "}
+          </a>
+          and is open-sourced on{" "}
           <a
             href="https://github.com/nija10/sky-react"
             target="_blank"
             rel="noreferrer"
           >
             Github
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://sky-react101.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
           </a>
         </footer>
       </div>
